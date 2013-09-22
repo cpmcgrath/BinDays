85f4b5b8d6dde63c358fea932cf79ae8cbb1b35c
Chris McGrath <admin@cpmcgrath.com>
2013-09-22 22:07:44 +1000

    Added description.

--- BinDays.Business/Data/BinType.cs
+++ BinDays.Business/Data/BinType.cs
@@ -1,10 +1,12 @@
 using System;
+using System.ComponentModel;
 
 namespace CMcG.BinDays
 {
     [Flags]
     public enum BinType
     {
+        [Description("General Waste")]
         GeneralWaste = 1,
         Recycling    = 2,
         Green        = 4
